97c2e5d06f8ae1092b35844acca6e23d028d4afa
dht <dht@users.noreply.github.com>
2023-01-18 07:08:52 +0200

    feat: show one resolution at a time closes #1

--- packages/block-viewer/src/components/Resolutions/Resolutions.style.tsx
+++ packages/block-viewer/src/components/Resolutions/Resolutions.style.tsx
@@ -8,4 +8,11 @@ export const Content = styled.div`
     height: 100vh;
     display: flex;
     background-color: #223;
+    flex-direction: column;
 `;
+
+export const Header = styled.div`
+    padding: 10px;
+`;
+
+export const IFrame = styled.iframe``;
